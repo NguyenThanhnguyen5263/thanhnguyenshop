a929d2c68b82565700d2195cd2c368883ed6f509
DESKTOP-4Q7T7C0\JAVA <designerthanhnguyen@gmail.com>
2018-04-03 11:00:12 +0700

    update 5 table model

---
+++ Model/Model/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Model
+{
+    [Table("Menus")]
+   public class Menu
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+        [Required]
+        public string Name { set; get; }
+        [Required]
+        public string URL { set; get; }
+
+        public int? DisplayOrder { set; get; } // allow null 
+        [Required] 
+        public int MenuGroupID { set; get; }
+
+        [ForeignKey("MenuGroupID")]
+        public virtual MenuGroup Menugroup { set; get; }
+
+        public string Target { set; get; }
+        [Required]
+        public bool Status { set; get; }
+    }
+}
